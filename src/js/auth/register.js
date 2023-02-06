acdb5ac7755f0beaaca9f8237594ba31a5bab843
Rohit kumar Amdahl <rohit_designer@outlook.com>
2023-02-06 15:09:33 +0100

    add variables

---
+++ src/js/auth/register.js
@@ -0,0 +1,3 @@
+const API_BASE_URL = "https://nf-api.onrender.com/api/v1/auction/auth/register";
+const registerForm = document.querySelector("#register-form");
+console.log(registerForm);
--- src/js/register.js
+++
@@ -1,1 +0,0 @@
-const API_BASE_URL = "https://nf-api.onrender.com";
