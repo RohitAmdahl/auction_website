0b0a15e241fe079198235bfbe70f655dd08e76a4
Rohit kumar Amdahl <rohit_designer@outlook.com>
2023-02-07 19:38:05 +0100

    id added to form

--- src/js/auth/login.js
+++ src/js/auth/login.js
@@ -1,11 +1,13 @@
+import * as storage from "../storage/localStorage.js";
 const loginUrl = "https://nf-api.onrender.com/api/v1/auction/auth/login";
+const method = "post";
 
-export async function login(loginUrl, data) {
+export async function login(url, data) {
   try {
     const postData = {
-      method: "post",
+      method,
       headers: {
-        "Content-type": "application/json; charset=UTF-8",
+        "Content-Type": "application/json",
       },
       body: JSON.stringify(data),
     };
@@ -14,34 +16,15 @@ export async function login(loginUrl, data) {
     const results = await response.json();
 
     const accessToken = results.accessToken;
-
     if (accessToken) {
-      window.location.replace("/profile.html");
+      window.location.replace("/index.html");
       console.log("I got a token, and should be logged in");
     } else {
       console.log("I did not get a token, so either a wrong email or password");
     }
     localStorage.setItem("Token", accessToken);
-
-    saveItem("profile", user);
+    saveItem("profile", results);
   } catch (error) {
     console.log(error);
   }
 }
-
-function logINform() {
-  const form = document.querySelector("#loginForm");
-  console.log(form);
-  form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const form = e.target;
-    const email = form.email.value;
-    const password = form.password.value;
-    const loginForm = { form, email, password };
-    form.reset();
-
-    login(loginUrl, loginForm);
-  });
-}
-
-logINform();
