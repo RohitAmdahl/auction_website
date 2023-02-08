6306e0ac4d311c55b4bca9e6d5d24bf70acb4180
Rohit kumar Amdahl <rohit_designer@outlook.com>
2023-02-08 15:11:44 +0100

    avatar.js

--- src/js/pages/avatar.js
+++ src/js/pages/avatar.js
@@ -1,31 +1,35 @@
+import { save } from "../storage/localStorage.js";
 export const avatarURL = `https://nf-api.onrender.com/api/v1/auction/profiles`;
+const user = JSON.parse(localStorage.getItem("user"));
+const profileName = user.name;
 
 export async function editAvatar(avatarImage) {
+  const token = localStorage.getItem("Token");
   try {
     const sendData = {
       avatar: avatarImage,
     };
     console.log(sendData);
 
-    const followData = {
+    const Data = {
       method: "put",
       headers: {
         "Content-type": "application/json ",
+        Authorization: `Bearer ${token}`,
       },
       body: JSON.stringify(sendData),
     };
 
-    const response = await fetch(
-      `${avatarURL}/${profileName}/media`,
-      followData
-    );
+    const response = await fetch(`${avatarURL}/${profileName}/media`, Data);
     console.log(response);
     const json = await response.json();
     console.log(json);
-
-    const avatar = json.avatar;
-    console.log(avatar);
-    localStorage.setItem("profile", avatar);
+    save("user", {
+      avatar: json.avatar,
+    });
+    document.querySelector("#profile_img").src = json.avatar;
+    user.avatar = json.avatar;
+    localStorage.setItem("user", JSON.stringify(user));
   } catch (error) {
     console.log(error);
   }
