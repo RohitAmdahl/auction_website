e963194d879c7f2c5604baf0aa8fb10e06973b9c
Rohit kumar Amdahl <rohit_designer@outlook.com>
2023-02-09 16:00:33 +0100

    working on bid history page on specific page

--- src/js/pages/biderhistory.js
+++ src/js/pages/biderhistory.js
@@ -1,12 +1,14 @@
 const base_Url = `https://nf-api.onrender.com/`;
-const listing_Url = `api/v1/auction/listing`;
+const listing_Url = `api/v1/auction/listings`;
 // const user = JSON.parse(localStorage.getItem("user"));
 // const profileName = user.name;
+const profile = document.querySelector(".profile-info");
 const queryString = document.location.search;
 const params = new URLSearchParams(queryString);
 console.log(params);
 const id = params.get("id");
 console.log(id);
+
 async function biderHistory() {
   const token = localStorage.getItem("Token");
   try {
@@ -25,6 +27,16 @@ async function biderHistory() {
     console.log(response);
     const json = await response.json();
     console.log(json);
+    // thumbnail for profile
+    const img = document.querySelector(".sellerPicture");
+    console.log(img);
+    img.src = json.seller.avatar;
+    const profileName = document.querySelector("#profileName");
+    profileName.innerHTML = json.seller.name;
+    const profileEmail = document.querySelector("#emailName");
+    profileEmail.innerHTML = json.seller.email;
+    const winner = document.querySelector("#winner");
+    winner.innerHTML = json.wins;
   } catch (error) {
     console.log(error);
   }
