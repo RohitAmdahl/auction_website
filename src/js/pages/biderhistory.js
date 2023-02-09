ec09ecc2a856508d3e098c6b9c5cb9235afdde58
Rohit kumar Amdahl <rohit_designer@outlook.com>
2023-02-09 18:04:01 +0100

    fetched seller profile

--- src/js/pages/biderhistory.js
+++ src/js/pages/biderhistory.js
@@ -5,9 +5,8 @@ const listing_Url = `api/v1/auction/listings`;
 const profile = document.querySelector(".profile-info");
 const queryString = document.location.search;
 const params = new URLSearchParams(queryString);
-console.log(params);
+
 const id = params.get("id");
-console.log(id);
 
 async function biderHistory() {
   const token = localStorage.getItem("Token");
@@ -24,19 +23,17 @@ async function biderHistory() {
       `${base_Url}${listing_Url}/${id}/?_seller=true&_bids=true`,
       Data
     );
-    console.log(response);
+
     const json = await response.json();
     console.log(json);
     // thumbnail for profile
     const img = document.querySelector(".sellerPicture");
-    console.log(img);
+
     img.src = json.seller.avatar;
     const profileName = document.querySelector("#profileName");
     profileName.innerHTML = json.seller.name;
     const profileEmail = document.querySelector("#emailName");
     profileEmail.innerHTML = json.seller.email;
-    const winner = document.querySelector("#winner");
-    winner.innerHTML = json.wins;
   } catch (error) {
     console.log(error);
   }
