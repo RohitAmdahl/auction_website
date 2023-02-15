680884c6c663e3c9533cf3bf7f94bb4f4ad653dc
Rohit kumar Amdahl <rohit_designer@outlook.com>
2023-02-15 23:10:46 +0100

    update

--- src/js/pages/explore.js
+++ src/js/pages/explore.js
@@ -2,7 +2,6 @@
 const cardsUrl = `https://nf-api.onrender.com/api/v1/auction/listings?_active=true`;
 const auctionProducts = document.querySelector("#auction");
 const loading = document.querySelector(".loading");
-// let product = [];
 
 async function auctionCards() {
   try {
@@ -36,10 +35,40 @@ async function auctionCards() {
               </div>
             </div>`;
     });
-
-    console.log(results);
   } catch (error) {
     console.log(error);
   }
 }
 auctionCards();
+//---------------------------------------------------------
+//---------------------------------------------------------
+//---------------------------------------------------------
+//---------------------------------------------------------
+//---------------------------------------------------------
+//---------------------------------------------------------
+function CreateHtmlCards() {
+  const container = document.createElement("div");
+  container.classList.add("rohit");
+  container.innerHTML = `<div class="card explore-cards">
+                <img
+                  src="}"
+                  class="card-img-top explore-media"
+                />
+                <div class="card-body">
+                  <ul class="list-group list-group-flush">
+                    <li class="list-group-item title"></li>
+                    <li class="list-group-item lead bid"> </li>
+                    <li class="list-group-item date"> </li>
+                  </ul>
+                  <a  class="btn big-btn btn-sm d-flex justify-content-center p-2 rounded-2 fw-bold mt-3"
+                   href="#">View item</a>
+                </div>
+              </div>`;
+  container.querySelector(".title").innerText = "hello world";
+  container.querySelector(".bid").innerText = "hello world";
+  container.querySelector(".date").innerText = "hello world";
+  console.log(container);
+  auctionProducts.append(container);
+  document.append(container);
+}
+CreateHtmlCards();
--- src/js/user/endAt.js
+++ src/js/user/endAt.js
@@ -1,6 +1,9 @@
 // "?_active=true&sort=endsAt&sortOrder=asc";
 const endsAt_url = `https://nf-api.onrender.com/api/v1/auction/listings?_active=true&sort=endsAt&sortOrder=asc`;
-
+//
+const endingAt = document.querySelector(".today");
+console.log(endingAt);
+//
 async function endsTime() {
   const token = localStorage.getItem("Token");
   try {
@@ -11,16 +14,32 @@ async function endsTime() {
         Authorization: `Bearer ${token}`,
       },
     };
-    const response = await fetch(endsAt_url);
+    const response = await fetch(endsAt_url, data);
     console.log(response);
     const json = await response.json();
     console.log(json);
-    for (let i = 0; i < json.length; i++) {
-      const element = json[i];
-      if (element === 10) {
-        break;
-      }
-    }
+
+    json.forEach((ListElement) => {
+      const endDiv = document.querySelector("div");
+      Div.classList.add(
+        "col-12",
+        "col-lg-4",
+        "col-md-6",
+        "col-sm-12",
+        "mt-4",
+        "mb-5"
+      );
+      const img = document.querySelector("img");
+      img.classList.add("card-img-top", "explore-media", "p-5");
+      const divBody = document.querySelector("div");
+      const span = document.querySelector("span");
+
+      endingAt.append(endDiv);
+      endDiv.append(img);
+      divBody.append(span);
+    });
+
+    //
   } catch (error) {
     console.log(error);
   }
