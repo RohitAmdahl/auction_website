d1964469cc0649eb4dd1eada0a30b7175bf25add
Rohit kumar Amdahl <rohit_designer@outlook.com>
2023-02-17 00:21:05 +0100

    update code

--- src/js/auth/login.js
+++ src/js/auth/login.js
@@ -8,7 +8,6 @@ export async function login(url, data) {
       method,
       headers: {
         "Content-Type": "application/json",
-        // Authorization: `Bearer ${token}`,
       },
       body: JSON.stringify(data),
     };
@@ -23,11 +22,6 @@ export async function login(url, data) {
     const credit = results.credits;
     localStorage.setItem("credits", credit);
 
-    // const avatarPic = setItem("avatar", avatar);
-    // const avatar = results.avatarPic;
-
-    // const profile = results.profile;
-    // localStorage.setItem("profile", profile);
     save("user", {
       name: results.name,
       email: results.email,
@@ -38,12 +32,6 @@ export async function login(url, data) {
     if (response.ok) {
       window.location.replace("/profile.html");
     }
-
-    // if (accessToken) {
-    //   window.location.replace("/profile.html");
-    // } else {
-    //   alert, "something went wrong";
-    // }
   } catch (error) {
     console.log(error);
   }
