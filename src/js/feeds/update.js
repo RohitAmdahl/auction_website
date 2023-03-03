939ee61fbbb14e37b8b0a3b272d3f743baf6c5be
Rohit kumar Amdahl <rohit_designer@outlook.com>
2023-03-03 08:41:31 +0100

    update

--- src/js/feeds/update.js
+++ src/js/feeds/update.js
@@ -3,7 +3,9 @@ export const updateListingUrl = `https://nf-api.onrender.com/api/v1/auction/list
 const url = new URL(location.href);
 const id = url.searchParams.get("id");
 console.log(id);
-
+/**
+ *
+ */
 export async function updateListingContent(create) {
   console.log(create);
   console.log(create.tags);
