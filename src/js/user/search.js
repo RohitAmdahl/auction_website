448516c50dbee12999a5742faf0e12c1ff098bd7
Rohit kumar Amdahl <rohit_designer@outlook.com>
2023-03-03 12:32:11 +0100

    remove code

--- src/js/user/search.js
+++ src/js/user/search.js
@@ -1,65 +1,67 @@
 import { ProfileLogOut } from "../handler/logout.js";
-const searchUrl = "https://nf-api.onrender.com/api/v1/auction/listings?_tag";
-const searchForm = document.querySelector("#searchForm");
-const input = document.querySelector("#inputvalue");
-const cardsDiv = document.querySelector("#auction");
+// const searchUrl = "https://nf-api.onrender.com/api/v1/auction/listings?_tag";
+// const searchForm = document.querySelector("#searchForm");
+// const input = document.querySelector("#inputvalue");
+// const cardsDiv = document.querySelector("#auction");
 
-console.log(term);
-async function searchApiCall() {
-  try {
-    const data = {
-      headers: {
-        "Content-Type": "application/json; charset=UTF-8",
-        Authorization: `Bearer ${token}`,
-      },
-    };
+// console.log(searchResults);
+// async function searchApiCall() {
+//   try {
+//     const data = {
+//       headers: {
+//         "Content-Type": "application/json; charset=UTF-8",
+//         Authorization: `Bearer ${token}`,
+//       },
+//     };
 
-    const response = await fetch(`${searchUrl}=${input.value}`, data); // .toLowerCase()
-    if (!response.ok) {
-      throw new Error(`HTTP error: ${response.status}`);
-    }
-    const results = await response.json();
-    cardsDiv.innerHTML = "";
-    results.forEach((element) => {
-      const endTime = new Date(element.endsAt).toLocaleTimeString("en-GB", {
-        hour: "2-digit",
-        minute: "2-digit",
-      });
-      cardsDiv.innerHTML += `<div class="col-12 col-lg-4 col-md-6 col-sm-12 mt-4">
-              <div class="card explore-cards">
-                <img
-                  src="${element.media[0]}"
-                  class="card-img-top explore-media"
-                />
-                <div class="card-body">
-                  <ul class="list-group list-group-flush">
-                    <li class="list-group-item">Title ;- ${element.title}</li>
-                    <li class="list-group-item lead">Bid :-  ${
-                      element._count.bids
-                    }</li>
-                    <li class="list-group-item">
-                    ${new Date(element.endsAt).toLocaleDateString()}</li>
-                     <li class="list-group-item">Finishing Time: ${endTime}</li>
-                  </ul>
-                  <a  class="btn big-btn btn-sm d-flex justify-content-center p-2 rounded-2 fw-bold mt-3"
-                   href="specific.html?id=${element.id}">View item</a>
-                </div>
-              </div>
-            </div>`;
-    });
-  } catch (error) {
-    console.log(error);
-  }
-}
+//     const response = await fetch(`${searchUrl}=${input.value}`, data); // .toLowerCase()
+//     if (!response.ok) {
+//       throw new Error(`HTTP error: ${response.status}`);
+//     }
+//     const results = await response.json();
+//     cardsDiv.innerHTML = "";
+//     searchResults = results.map((element) => {
+//       const endTime = new Date(element.endsAt).toLocaleTimeString("en-GB", {
+//         hour: "2-digit",
+//         minute: "2-digit",
+//       });
+//       cardsDiv.innerHTML += `<div class="col-12 col-lg-4 col-md-6 col-sm-12 mt-4">
+//               <div class="card explore-cards">
+//                 <img
+//                   src="${element.media[0]}"
+//                   class="card-img-top explore-media"
+//                 />
+//                 <div class="card-body">
+//                   <ul class="list-group list-group-flush">
+//                     <li class="list-group-item">Title ;- ${element.title}</li>
+//                     <li class="list-group-item lead">Bid :-  ${
+//                       element._count.bids
+//                     }</li>
+//                     <li class="list-group-item">
+//                     ${new Date(element.endsAt).toLocaleDateString()}</li>
+//                      <li class="list-group-item">Finishing Time: ${endTime}</li>
+//                   </ul>
+//                   <a  class="btn big-btn btn-sm d-flex justify-content-center p-2 rounded-2 fw-bold mt-3"
+//                    href="specific.html?id=${element.id}">View item</a>
+//                 </div>
+//               </div>
+//             </div>`;
+//     });
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
 
-searchForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const searchForm = e.target;
-  console.log(searchForm);
-  console.log(searchForm);
+// searchForm.addEventListener("submit", (e) => {
+//   e.preventDefault();
+//   const searchForm = e.target;
+//   console.log(searchForm);
+//   console.log(searchForm);
 
-  searchApiCall();
-});
+//   searchApiCall();
+// });
+
+// ---menu hide and show
 const token = localStorage.getItem("Token");
 const signup = document.querySelector(".cta-signup");
 const login = document.querySelector(".cta-login");
